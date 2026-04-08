878097c9c45bc2d536439b0434a20dbc2abcfe46
siedzetutaj <121103257+siedzetutaj@users.noreply.github.com>
2026-04-08 15:35:06 +0200

    SO setup for update enemyAI

--- Assets/Scripts/EnemieAI/EnemieSO.cs
+++ Assets/Scripts/EnemieAI/EnemieSO.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "EnemieSO", menuName = "Scriptable Objects/Enemies/EnemieSO")]
 public class EnemieSO : ScriptableObject
 {
+    public int Health;
+    public int Power;
     public List<EnemieBuildingSO> BuildingsToSpawn = new();
     //Trzeba przerobiæ budynki ¿eby spawonowa³y enemiesów
 }
