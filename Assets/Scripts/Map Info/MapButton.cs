cc69405434e7f760b7ebc359cebf550e7acbf8a0
smasherboy47 <97182478+smasherboy47@users.noreply.github.com>
2026-04-12 21:39:25 +0200

    Podstawowe Generowanie Poziomu

--- Assets/Scripts/Map Info/MapButton.cs
+++ Assets/Scripts/Map Info/MapButton.cs
@@ -1,9 +1,22 @@
+using System.Collections.Generic;
 using UnityEngine;
+
+[System.Serializable]
+public class MapConnection
+{
+    public MapButton Button;
+    public RectTransform ConnectionGraphic;
+
+    public MapConnection(MapButton newButton, RectTransform conGraphic)
+    {
+        Button = newButton;
+        ConnectionGraphic = conGraphic;
+    }
+}
 
 public class MapButton : MonoBehaviour
 {
-    [field: SerializeField] public Vector2Int Pos { get; private set; }
-    [field: SerializeField] public MapButton Left { get; private set; }
-    [field: SerializeField] public MapButton Middle { get; private set; }
-    [field: SerializeField] public MapButton Right { get; private set; }
+    [field: SerializeField] public Vector2Int Pos { get; set; }
+    public HashSet<int> PossibleConnections = new();
+    public List<MapConnection> Connections = new();
 }
