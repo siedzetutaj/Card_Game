14285d39a1c7b7241482a5bbfa05e305d70d2df5
maciusk8 <mikolajek.maciek@gmail.com>
2026-05-22 20:19:18 +0200

    add range marker to shoot efect

--- Assets/Scripts/Buildings/BuildingsEffects/ShootBuildingEffectSO.cs
+++ Assets/Scripts/Buildings/BuildingsEffects/ShootBuildingEffectSO.cs
@@ -6,9 +6,21 @@ public class ShootBuildingEffectSO : BuildingEffectSO
     [SerializeField] private int _damage;
     [SerializeField] private int _range;
     [SerializeField] private float _attackSpeed;
+    [SerializeField] private GameObject _rangeMarkerPrefab;
 
     public override void ApplyBuildingEffect(BuildingHandler buildingHandler)
     {
+        // Spawn RangeMarker as a child of the building if prefab is assigned
+        if (_rangeMarkerPrefab != null && buildingHandler.GetComponentInChildren<RangeMarker>(true) == null)
+        {
+            var marker = Instantiate(_rangeMarkerPrefab, buildingHandler.transform);
+            var rangeMarker = marker.GetComponent<RangeMarker>();
+            if (rangeMarker != null)
+            {
+                rangeMarker.DisableVisiblity();
+            }
+        }
+
         BuildingShooter shooter = buildingHandler.GetComponent<BuildingShooter>();
         if (shooter == null)
         {
